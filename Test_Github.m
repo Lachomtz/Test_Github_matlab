a729d9dbae25f3f926786697faac88287517323b
Lachomtz <horacio.mtz.cal@gmail.com>
2018-04-22 21:32:59 +0200

    Add files via upload
    This is the file that is going to be used for the test.

---
+++ Test_Github.m
@@ -0,0 +1,16 @@
+%Author: Horacio Martínez Calderón
+%Date: 22.04.2018
+%Description: Test github.
+
+ close all
+ clear all
+ clc
+ 
+ %% Program
+ 
+ N=5;
+ for i=1:N
+ 
+     display('Rola es hot');
+
+ end
